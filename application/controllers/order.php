e256b126095371067bcb3f718df05af10d1b9d9c
Rizwan Mehmood <Rizwan Mehmood>
2016-10-30 06:15:18 +0500

    Updates 30 October

--- application/controllers/order.php
+++ application/controllers/order.php
@@ -2,23 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class order extends CI_Controller {
-	
 	public function __construct() {
         parent::__construct();
         if (!($this->session->userdata('user_id'))) {
             redirect(base_url().'admin/login');
         }
     }
-
-	public function index()
-	{
-		redirect(base_url().'order/orderList');		
-	}
-	public function orderList()
-	{
-		$this->load->view('admin/order/order_list');
-
-	}
 	
 	public function index()
 	{
@@ -29,7 +18,18 @@ class order extends CI_Controller {
 	
 	public function orderList()
 	{
-			$this->load->view('admin/order/order_list');
-
+		$sql='select o.OrderId,c.Name,m.MenuName,o.Quantity,o.PaymentMethod,o.PaymentCycleMethod,o.Status,o.DaysofWeek,o.StartDate,o.ReceivedDate,o.LastUpdatedDate,o.DueDate from orders o join customers c on o.CustomerId=c.CustomerID join menu m on m.menuID= o.MenuId';
+			$result=$this->Common_Model->execute_exact_string($sql);
+			$data['orders']=$result;
+			$this->load->view('admin/order/order_list',$data);
+		
+	}
+	public function addOrderDetails($order_id)
+	{
+		
+		$this->load->view('admin/order/add_order_details');
+		
+		
+		
 	}
 }
